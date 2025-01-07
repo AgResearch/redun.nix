af93c5beff32e9177ae92187c49756299772b7c7
Simon Guest <simon.guest@agresearch.co.nz>
2025-01-07 16:41:45 +1300

    Tidy up

---
+++ README.md
@@ -0,0 +1,5 @@
+# redun.nix
+
+Provides the Python redun package as a Nix Python package.
+
+_Work in progress_
--- default.nix
+++ default.nix
@@ -22,14 +22,6 @@
   name = "redun";
   version = "0.22.0";
 
-  # mkDerivation = {
-  #   nativeBuildInputs = [
-  #     config.deps.python311Packages.setuptools
-  #   ];
-  #   # propagatedBuildInputs = [
-  #   # ];
-  # };
-
   buildPythonPackage = {
     pyproject = true;
 
@@ -51,111 +43,121 @@
       config.deps.gcc
     ];
 
-    overrides = {
-      aiohappyeyeballs = {
-        buildPythonPackage.pyproject = true;
-        mkDerivation.nativeBuildInputs = [
-          config.deps.python311Packages.poetry-core
-        ];
-      };
-      aioitertools = {
-        buildPythonPackage.pyproject = true;
-        mkDerivation.nativeBuildInputs = [
-          config.deps.python311Packages.flit
-        ];
-      };
-      attrs = {
-        buildPythonPackage.pyproject = true;
-        mkDerivation = {
-          patches = [ ./patches/attrs-24.3.0.license-files.patch ];
+    overrides =
+      {
+        aiohappyeyeballs = {
+          buildPythonPackage.pyproject = true;
+          mkDerivation.nativeBuildInputs = [
+            config.deps.python311Packages.poetry-core
+          ];
+        };
+        aioitertools = {
+          buildPythonPackage.pyproject = true;
+          mkDerivation.nativeBuildInputs = [
+            config.deps.python311Packages.flit
+          ];
+        };
+        attrs = {
+          buildPythonPackage.pyproject = true;
+          mkDerivation = {
+            patches = [ ./patches/attrs-24.3.0.license-files.patch ];
 
-          nativeBuildInputs = [
+            nativeBuildInputs = [
+              config.deps.python311Packages.hatchling
+              config.deps.python311Packages.hatch-vcs
+              config.deps.python311Packages.hatch-fancy-pypi-readme
+            ];
+          };
+        };
+        colorama = {
+          buildPythonPackage.pyproject = true;
+          mkDerivation = {
+            nativeBuildInputs = [
+              config.deps.python311Packages.hatchling
+            ];
+          };
+        };
+        fancycompleter = {
+          buildPythonPackage.pyproject = true;
+          mkDerivation = {
+            patches = [ ./patches/fancycompleter-0.9.1.pyproject.patch ];
+
+            nativeBuildInputs = [
+              config.deps.python311Packages.distutils
+            ];
+          };
+        };
+        frozenlist = {
+          buildPythonPackage.pyproject = true;
+          mkDerivation.nativeBuildInputs = [
+            config.deps.python311Packages.expandvars
+            config.deps.python311Packages.cython
+          ];
+        };
+        fsspec = {
+          buildPythonPackage.pyproject = true;
+          mkDerivation.nativeBuildInputs = [
             config.deps.python311Packages.hatchling
             config.deps.python311Packages.hatch-vcs
-            config.deps.python311Packages.hatch-fancy-pypi-readme
           ];
         };
-      };
-      colorama = {
-        buildPythonPackage.pyproject = true;
-        mkDerivation = {
-          nativeBuildInputs = [
+        idna = {
+          buildPythonPackage.pyproject = true;
+          mkDerivation.nativeBuildInputs = [
+            config.deps.python311Packages.flit
+          ];
+        };
+        markdown-it-py = {
+          buildPythonPackage.pyproject = true;
+        };
+        mdit-py-plugins = {
+          buildPythonPackage.pyproject = true;
+          mkDerivation.nativeBuildInputs = [
+            config.deps.python311Packages.flit
+          ];
+          # mkDerivation.buildInputs = [
+          #   config.pip.drvs.markdown-it-py.public
+          # ];
+        };
+        mdurl = {
+          buildPythonPackage.pyproject = true;
+          mkDerivation.nativeBuildInputs = [
+            config.deps.python311Packages.flit
+          ];
+        };
+        platformdirs = {
+          buildPythonPackage.pyproject = true;
+          mkDerivation.nativeBuildInputs = [
             config.deps.python311Packages.hatchling
-            # config.deps.python311Packages.hatch-vcs
-            # config.deps.python311Packages.hatch-fancy-pypi-readme
+            config.deps.python311Packages.hatch-vcs
+          ];
+        };
+        propcache = {
+          buildPythonPackage.pyproject = true;
+          mkDerivation.nativeBuildInputs = [
+            config.deps.python311Packages.expandvars
+            config.deps.python311Packages.cython
+          ];
+        };
+        pygments = {
+          buildPythonPackage.pyproject = true;
+          mkDerivation.nativeBuildInputs = [
+            config.deps.python311Packages.hatchling
+          ];
+        };
+        typing-extensions = {
+          buildPythonPackage.pyproject = true;
+          mkDerivation.nativeBuildInputs = [
+            config.deps.python311Packages.flit
+          ];
+        };
+        urllib3 = {
+          buildPythonPackage.pyproject = true;
+          mkDerivation.nativeBuildInputs = [
+            config.deps.python311Packages.hatchling
+            config.deps.python311Packages.hatch-vcs
           ];
         };
       };
-      fancycompleter = {
-        buildPythonPackage.pyproject = true;
-        mkDerivation = {
-          patches = [ ./patches/fancycompleter-0.9.1.pyproject.patch ];
-
-          nativeBuildInputs = [
-            config.deps.python311Packages.distutils
-          ];
-        };
-      };
-      frozenlist = {
-        buildPythonPackage.pyproject = true;
-        mkDerivation.nativeBuildInputs = [
-          config.deps.python311Packages.expandvars
-          config.deps.python311Packages.cython
-        ];
-      };
-      fsspec = {
-        buildPythonPackage.pyproject = true;
-        mkDerivation.nativeBuildInputs = [
-          config.deps.python311Packages.hatchling
-          config.deps.python311Packages.hatch-vcs
-        ];
-      };
-      idna = {
-        buildPythonPackage.pyproject = true;
-        mkDerivation.nativeBuildInputs = [
-          config.deps.python311Packages.flit
-        ];
-      };
-      mdurl = {
-        buildPythonPackage.pyproject = true;
-        mkDerivation.nativeBuildInputs = [
-          config.deps.python311Packages.flit
-        ];
-      };
-      platformdirs = {
-        buildPythonPackage.pyproject = true;
-        mkDerivation.nativeBuildInputs = [
-          config.deps.python311Packages.hatchling
-          config.deps.python311Packages.hatch-vcs
-        ];
-      };
-      propcache = {
-        buildPythonPackage.pyproject = true;
-        mkDerivation.nativeBuildInputs = [
-          config.deps.python311Packages.expandvars
-          config.deps.python311Packages.cython
-        ];
-      };
-      pygments = {
-        buildPythonPackage.pyproject = true;
-        mkDerivation.nativeBuildInputs = [
-          config.deps.python311Packages.hatchling
-          # config.deps.python311Packages.hatch-vcs
-        ];
-      };
-      typing-extensions = {
-        buildPythonPackage.pyproject = true;
-        mkDerivation.nativeBuildInputs = [
-          config.deps.python311Packages.flit
-        ];
-      };
-      urllib3 = {
-        buildPythonPackage.pyproject = true;
-        mkDerivation.nativeBuildInputs = [
-          config.deps.python311Packages.hatchling
-          config.deps.python311Packages.hatch-vcs
-        ];
-      };
-    };
   };
 }
