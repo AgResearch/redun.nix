bcbf609ca2f3b2e3e2676d2ba584efba17f74eb3
Simon Guest <simon.guest@agresearch.co.nz>
2025-01-21 12:05:16 +1300

    Include parameterized python sire-packages

--- flake.nix
+++ flake.nix
@@ -49,13 +49,23 @@
             postBuild = ''
               # edit out redun's own python for our parameterized python
               for editpath in bin/redun bin/.redun-wrapped nix-support/propagated-build-inputs; do
-                sed -i -e "s,/nix/store/[a-z0-9]*-python3-[.0-9]*,${python},g" $out/$editpath
+                sed -i -e "s:/nix/store/[a-z0-9]*-python3-[.0-9]*:${python}:g" $out/$editpath
+              done
+
+              # and add parameterized python site-packages  to addsitedir
+              for editpath in bin/.redun-wrapped; do
+                sed -i -e "s:/site-packages']:XYZ/site-packages','${python}/lib/${python.libPrefix}/site-packages']:" $out/$editpath
               done
             '';
           }
         ;
       in
       {
-        lib.redun = redun-wrapped;
+        # if you want redun to use your own python which has particular packages installed,
+        # use this attribute, like lib.${system}.default { python = python-with-my-packages; }
+        lib.default = redun-wrapped;
+
+        # otherwise this one is the same, but with the default python with no extra packages
+        packages.default = redun-wrapped { python = pkgs.python3; };
       });
 }
